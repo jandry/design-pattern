0d2f309ce66acf61429db1404a80541af6e1d5bf
BARRY Guillaume <guillaume.barry@soprabanking.com>
2019-12-22 19:11:59 +0100

    Change to Jedi

--- src/test/java/com/tetras/FactoryTest.java
+++ src/test/java/com/tetras/FactoryTest.java
@@ -1,6 +1,5 @@
 package com.tetras;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -8,20 +7,18 @@ import org.junit.jupiter.api.Test;
 public class FactoryTest {
 
     @Test
-    public void testOracle() {
-        OracleCommandExecutor executor = new OracleCommandExecutor();
-        assertTrue(executor instanceof OracleCommandExecutor);
-        executor.connect("localhost", 1521, "toto", "password");
-        assertNotNull(executor.execute("query"));
-        executor.close();
+    public void testYoda() {
+        Yoda executor = new Yoda();
+        assertTrue(executor instanceof Yoda);
+        executor.mediter();
+        executor.combattre();
     }
 
     @Test
-    public void testMariaDb() {
-        MariaDbCommandExecutor executor = new MariaDbCommandExecutor();
-        assertTrue(executor instanceof MariaDbCommandExecutor);
-        executor.connect("localhost", 1521, "toto", "password");
-        assertNotNull(executor.execute("query"));
-        executor.close();
+    public void testLukeSkywalker() {
+        LukeSkywalker executor = new LukeSkywalker();
+        assertTrue(executor instanceof LukeSkywalker);
+        executor.mediter();
+        executor.combattre();
     }
 }
