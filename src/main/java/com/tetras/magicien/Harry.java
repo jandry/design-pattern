f2664ad9fd731c331df3b7f902f576ef4e542aad
BARRY Guillaume <guillaume.barry@soprabanking.com>
2019-12-23 21:56:11 +0100

    Update uml with methods

--- src/main/java/com/tetras/magicien/Harry.java
+++ src/main/java/com/tetras/magicien/Harry.java
@@ -1,19 +1,9 @@
 package com.tetras.magicien;
 
-import com.tetras.ingredient.DoseUrineMusaraigne;
-import com.tetras.ingredient.FeuilleSecheTout;
-import com.tetras.materiel.PieceMolduArgent;
-import com.tetras.materiel.PieceOr;
-import com.tetras.materiel.Recipient;
-
 public class Harry {
 
     public void attendre(int minute) {
 
-    }
-
-    public PieceOr transformer(PieceMolduArgent piece) {
-        return new PieceOr();
     }
 
 }
