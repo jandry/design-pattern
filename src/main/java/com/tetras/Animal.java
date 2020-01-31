ead03f66e4c29b6883f2aa10bb1ef0169d302088
BARRY Guillaume <guillaume.barry@soprabanking.com>
2020-01-31 21:56:02 +0100

    minor simplification

--- src/main/java/com/tetras/Animal.java
+++ src/main/java/com/tetras/Animal.java
@@ -7,10 +7,6 @@ public class Animal {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Animal(String name) {
         this.name = name;
     }
