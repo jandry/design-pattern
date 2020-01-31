d432bff9b47d66cfa03501749bb627a27a94ee48
BARRY Guillaume <guillaume.barry@soprabanking.com>
2020-01-31 19:26:30 +0100

    Push test

--- src/test/java/com/tetras/PotionTest.java
+++ src/test/java/com/tetras/PotionTest.java
@@ -1,5 +1,12 @@
 package com.tetras;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.tetras.ingredient.DoseUrineMusaraigne;
+import com.tetras.materiel.PieceMolduArgent;
+import com.tetras.materiel.PieceOr;
+import com.tetras.materiel.Recipient;
+
 import org.junit.jupiter.api.Test;
 
 public class PotionTest {
@@ -10,6 +17,15 @@ public class PotionTest {
 
     @Test
     public void preparerPotionBrilleTout() {
+        PieceMolduArgent piece = new PieceMolduArgent();
+        Recipient recipient = new Recipient();
+        recipient.ajouter(piece);
+        DoseUrineMusaraigne dose1 = DoseUrineMusaraigne.prendre();
+        recipient.ajouter(dose1);
+        DoseUrineMusaraigne dose2 = DoseUrineMusaraigne.prendre();
+        recipient.ajouter(dose2);
+        attendre(5);
 
+        assertTrue(piece.transformer() instanceof PieceOr);
     }
 }
