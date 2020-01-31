eb475da931b6e57fce2e3bbabd5689afdb04b790
BARRY Guillaume <guillaume.barry@soprabanking.com>
2020-01-31 22:11:06 +0100

    Update proxy

--- src/main/java/com/tetras/SchtroumpfDouanier.java
+++ src/main/java/com/tetras/SchtroumpfDouanier.java
@@ -1,5 +1,10 @@
 package com.tetras;
 
 public class SchtroumpfDouanier extends Schtroumpf {
-
+    public String saluer(Animal animal) {
+        if (animal instanceof AnimalDangereux)
+                return "Au revoir " + animal.getName();
+            else 
+            return super.saluer(animal);
+    }
 }
