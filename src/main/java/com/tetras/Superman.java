bf184787cb04e6c43d47af164c9a51b10c5788c1
BARRY Guillaume <guillaume.barry@soprabanking.com>
2020-02-01 07:47:59 +0100

    Add failed test case long init

--- src/main/java/com/tetras/Superman.java
+++ src/main/java/com/tetras/Superman.java
@@ -5,6 +5,11 @@ public class Superman {
     private static Superman instance = new Superman();
 
     private Superman() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static Superman getInstance() {
