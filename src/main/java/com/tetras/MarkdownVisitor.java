1bca75a0022b20668012e10ecac2370ef36cbd45
BARRY Guillaume <guillaume.barry@soprabanking.com>
2020-02-15 19:54:23 +0100

    Add generique and implem

--- src/main/java/com/tetras/MarkdownVisitor.java
+++ src/main/java/com/tetras/MarkdownVisitor.java
@@ -1,20 +1,22 @@
 package com.tetras;
 
+import java.text.MessageFormat;
+
 public class MarkdownVisitor implements Visitor {
 
     @Override
     public void visit(Gras element) {
-        throw new UnsupportedOperationException("Not implemented");
+        System.out.println(MessageFormat.format("__{0}__", element.getTexte()));
     }
 
     @Override
     public void visit(Titre element) {
-        throw new UnsupportedOperationException("Not implemented");
+        System.out.println(MessageFormat.format("# {0}", element.getTexte()));
     }
 
     @Override
     public void visit(Liste element) {
-        throw new UnsupportedOperationException("Not implemented");
+        System.out.println(MessageFormat.format("* {0}", element.getTexte()));
     }
 
     @Override
