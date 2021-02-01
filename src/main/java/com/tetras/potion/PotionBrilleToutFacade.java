6a1918ee092ca65b36632d258a69c4dec39d98fc
BARRY Guillaume <guillaume.barry@soprabanking.com>
2021-02-01 19:07:58 +0100

    Enhance

--- src/main/java/com/tetras/potion/PotionBrilleToutFacade.java
+++ src/main/java/com/tetras/potion/PotionBrilleToutFacade.java
@@ -1,6 +1,7 @@
 package com.tetras.potion;
 
 import com.tetras.ingredient.DoseUrineMusaraigne;
+import com.tetras.ingredient.FeuilleSecheTout;
 import com.tetras.materiel.PieceMolduArgent;
 import com.tetras.materiel.PieceOr;
 import com.tetras.materiel.Recipient;
@@ -10,14 +11,12 @@ public class PotionBrilleToutFacade {
     private static void attendre(int minute) {
     }
 
-    public static PieceOr transform(PieceMolduArgent piece) {
-        Recipient recipient = new Recipient();
+    public static PieceOr transform(PieceMolduArgent piece, Recipient recipient, DoseUrineMusaraigne dose1, DoseUrineMusaraigne dose2, FeuilleSecheTout feuille) {
         recipient.ajouter(piece);
-        DoseUrineMusaraigne dose1 = DoseUrineMusaraigne.prendre();
-        recipient.ajouter(dose1);
-        DoseUrineMusaraigne dose2 = DoseUrineMusaraigne.prendre();
-        recipient.ajouter(dose2);
+        piece.couvrir(dose1);
+        piece.couvrir(dose2);
         attendre(5);
+        piece.essuyer(feuille);
         return piece.transformer();
     }
 
--- src/test/java/com/tetras/PotionTest.java
+++ src/test/java/com/tetras/PotionTest.java
@@ -1,9 +1,13 @@
 package com.tetras;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.tetras.ingredient.DoseUrineMusaraigne;
+import com.tetras.ingredient.FeuilleSecheTout;
 import com.tetras.materiel.PieceMolduArgent;
 import com.tetras.materiel.PieceOr;
+import com.tetras.materiel.Recipient;
 import com.tetras.potion.PotionBrilleToutFacade;
 
 import org.junit.jupiter.api.Test;
@@ -12,7 +16,14 @@ public class PotionTest {
 
     @Test
     public void preparerPotionBrilleTout() {
-        PieceMolduArgent piece = new PieceMolduArgent();
-        assertTrue(PotionBrilleToutFacade.transform(piece) instanceof PieceOr);
+        PieceMolduArgent piece = PieceMolduArgent.prendre();
+        Recipient recipient = Recipient.prendre();
+        DoseUrineMusaraigne dose1 = DoseUrineMusaraigne.prendre();
+        DoseUrineMusaraigne dose2 = DoseUrineMusaraigne.prendre();
+        FeuilleSecheTout feuille = FeuilleSecheTout.prendre();
+        //
+        PieceOr result = PotionBrilleToutFacade.transform(piece, recipient, dose1, dose2,feuille);
+        //
+        assertNotNull(result);
     }
 }
