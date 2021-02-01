2576afc0a520ab862d39ab9ac1cbd3a5712f76c6
BARRY Guillaume <guillaume.barry@soprabanking.com>
2021-02-01 19:25:52 +0100

    Fix

--- src/main/java/com/tetras/SchtroumpfDouanier.java
+++ src/main/java/com/tetras/SchtroumpfDouanier.java
@@ -3,7 +3,7 @@ package com.tetras;
 public class SchtroumpfDouanier extends Schtroumpf {
     public String saluer(Animal animal) {
         if (animal instanceof AnimalDangereux)
-                return "Au revoir " + animal.getName();
+                return "Au revoir " + animal.getNom();
             else 
             return super.saluer(animal);
     }
