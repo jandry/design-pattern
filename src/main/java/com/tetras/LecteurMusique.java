779ab02f94f73c7cdbe0858ac48fe59ce56cdb9b
BARRY Guillaume <guillaume.barry@soprabanking.com>
2021-02-19 21:19:00 +0100

    Just test

--- src/main/java/com/tetras/LecteurMusique.java
+++ src/main/java/com/tetras/LecteurMusique.java
@@ -5,21 +5,12 @@ import java.util.List;
 
 public class LecteurMusique {
 
-    List<MementoMorceau> historiqueMorceau = new ArrayList<>();
-
     public String lire(Morceau morceau) {
-        historiqueMorceau.add(morceau.creerMemento());
         return morceau.getNom();
     }
 
     public String lireMorceauPrecedent() {
-        String result = "";
-        if (historiqueMorceau.size() >= 2) {
-            MementoMorceau memento = historiqueMorceau.get(historiqueMorceau.size() - 2);
-            Morceau morceau = new Morceau(memento);
-            result = morceau.getNom();
-        }
-        return result;
+        return "";
     }
 
 }
--- src/main/java/com/tetras/Morceau.java
+++ src/main/java/com/tetras/Morceau.java
@@ -15,16 +15,4 @@ public class Morceau {
         this.nom = nom;
     }
 
-    public Morceau(MementoMorceau memento) {
-        restaurerMemento(memento);
-    }
-
-    public MementoMorceau creerMemento() {
-        return new MementoMorceau(getNom());
-    }
-
-    public void restaurerMemento(MementoMorceau memento) {
-        setNom(memento.getNom());
-    }
-
 }
