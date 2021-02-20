a847d283f54d0934a6621e8882853fc4b6fe2f24
BARRY Guillaume <guillaume.barry@soprabanking.com>
2021-02-20 09:15:53 +0100

    fix compile

--- src/main/java/com/tetras/brawler/Frank.java
+++ src/main/java/com/tetras/brawler/Frank.java
@@ -3,9 +3,11 @@ package com.tetras.brawler;
 public class Frank {
 
     public String activerEponge() {
+        return "";
     }
 
     public String activerTourDeForce() {
+        return "";
     }
 
 }
