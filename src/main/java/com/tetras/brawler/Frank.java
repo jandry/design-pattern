ed26006736686363449561df997914babfb88685
BARRY Guillaume <guillaume.barry@soprabanking.com>
2021-02-20 09:14:40 +0100

    implem

--- src/main/java/com/tetras/brawler/Frank.java
+++ src/main/java/com/tetras/brawler/Frank.java
@@ -3,9 +3,11 @@ package com.tetras.brawler;
 public class Frank {
 
     public String activerEponge() {
+        return "obtient + 1000 PV max.";
     }
 
     public String activerTourDeForce() {
+        return "Les Brawlers tombés au combat laissent des squelettes violets derrière eux. Seul frank peut les voir, et en attraper un lui donne +30% de dégâts durant 10 secondes.";
     }
 
 }
