3a8fc5febf7bf1cdb581bf520a61ab60b4b09c0b
BARRY Guillaume <guillaume.barry@soprabanking.com>
2022-01-25 20:14:07 +0100

    Remove clone method

--- src/main/java/com/tetras/Clone.java
+++ src/main/java/com/tetras/Clone.java
@@ -14,23 +14,15 @@ public class Clone {
         this.nom = nom;
     }
 
-    public Clone() {
-        byte[] array = new byte[7];
-        new Random().nextBytes(array);
-        nom = new String(array, Charset.forName("UTF-8"));
-        System.out.println("Clone: " +getNom());
-
-    }
-
     public boolean equals(Object objectToCompare) {
-        if (objectToCompare==null)
+        if (objectToCompare == null)
             return false;
         if (!this.getClass().getName().equals(objectToCompare.getClass().getName())) {
             return false;
         }
         Clone cloneToCompare = (Clone) objectToCompare;
-        if (this.getNom()==null) {
-            return (cloneToCompare.getNom() ==null);
+        if (this.getNom() == null) {
+            return (cloneToCompare.getNom() == null);
         }
         return (this.getNom().equals(cloneToCompare.getNom()));
     }
