b0dbf4ee3b2c4e5204dcfb39d58eace4f979140d
BARRY Guillaume <guillaume.barry@soprabanking.com>
2022-04-06 18:36:09 +0200

    misspell

--- src/test/java/com/tetras/LecteurMusiqueTest.java
+++ src/test/java/com/tetras/LecteurMusiqueTest.java
@@ -9,12 +9,12 @@ public class LecteurMusiqueTest {
     @Test
     public void testLecture() {
         Morceau cannonball = new Morceau("cannonball");
-        Morceau loser = new Morceau("loser");
+        Morceau looser = new Morceau("loser");
         Morceau walkOnTheWildSide = new Morceau("walk on wild side");
         //
         LecteurMusique lecteur = new LecteurMusique();
         assertEquals(cannonball.getNom(), lecteur.lire(cannonball));
-        assertEquals(loser.getNom(), lecteur.lire(loser));
+        assertEquals(looser.getNom(), lecteur.lire(looser));
         assertEquals(walkOnTheWildSide.getNom(), lecteur.lire(walkOnTheWildSide));
     }
 }
